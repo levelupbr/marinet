48394d1bc26497246612f17db7d3563e1211b562
Jeferson Amorim <amorimjj@gmail.com>
2015-11-04 18:42:40 -0200

    Configurando ambientes

--- src/api/config/production.js
+++ src/api/config/production.js
@@ -1,17 +1,17 @@
 'use strict';
 
 module.exports = {
-    'db': 'mongodb://localhost/marinetdb',
+    'db': 'mongodb://10.20.1.29/marinetdb',
     'dbName': 'marinetdb',
     'account': {
         'defaultId': '005aeacc-d8ce-48c6-9ed8-cb89aa29bdad'
     },
-    'allowedOrigins': ['marinet.me'],
+    'allowedOrigins': ['marinet.levelup.com.br'],
     'marinet': {
         app: {
             key: 'ac0c0afe317621c1dfae6645bcf7d855b9ecf40f1162952ee3676edbba79f80b',
             id: '540a26f033026ce20a07ec33'
         },
-        rootUrl: 'http://api.marinet.me'
+        rootUrl: 'http://marinet.levelup.com.br'
     }
 }
