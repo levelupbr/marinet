6f4d60cd1b9d2c17a29cdd69717753f0a3e37a0b
ryomei <eduardo.nakamura@levelup.com.br>
2015-11-19 11:47:32 -0200

    add solve attempts

--- src/app/app/scripts/controllers/error.js
+++ src/app/app/scripts/controllers/error.js
@@ -13,7 +13,6 @@ angular.module('marinetApp')
                 $scope.solved = error.solved;
             });
 
-
             $scope.solve = function () {
                 toaster.pop('success', '', 'Erro solucionado');
                 Errors.solve($scope.hash, $scope.name).then(function (data) {
@@ -41,5 +40,12 @@ angular.module('marinetApp')
                     return $filter('date')(new Date(val), 'MMMM, dd yyyy HH:mm:ss');
 
                 return val;
+            };
+            
+            $scope.parseErrorValue = function(value){
+                if (Number.isInteger(value)){
+                    return value;
+                }                
+                return value.length <= 200 ? $scope.displayVal(value) : "<pre>" + value + "</pre>";
             };
     }]);
